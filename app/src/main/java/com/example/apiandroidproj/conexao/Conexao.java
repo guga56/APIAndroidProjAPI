68160ebd22e8b4add1e0aedec27d0fd5374f7224
usuario <gusatvogregorio086@gmail.com>
2019-06-05 21:43:13 -0300

    Initial

---
+++ app/src/main/java/com/example/apiandroidproj/conexao/Conexao.java
@@ -0,0 +1,62 @@
+package com.example.apiandroidproj.conexao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Conexao {
+
+    public static  String getJSONFromAPI(String url){
+        String retorno="";
+        try{
+
+            URL apiEnd = new URL(url);
+            int codigoResposta;
+            HttpURLConnection conexao;
+            InputStream is;
+
+            conexao = (HttpURLConnection) apiEnd.openConnection();
+            conexao.setRequestMethod("GET");
+            conexao.setReadTimeout(15000);
+            conexao.setConnectTimeout(15000);
+            conexao.connect();
+
+            codigoResposta = conexao.getResponseCode();
+            if(codigoResposta < HttpURLConnection.HTTP_BAD_REQUEST){
+                is = conexao.getInputStream();
+            }else{
+                is = conexao.getErrorStream();
+            }
+
+        }catch (MalformedURLException e){
+             e.printStackTrace();
+        }catch (IOException e){
+             e.printStackTrace();
+        }
+        return retorno;
+    }
+
+    private static String converterInputStreamToString(InputStream is){
+        StringBuffer buffer = new StringBuffer();
+        try{
+            BufferedReader br;
+            String linha;
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while((linha = br.readLine())!=null){
+                buffer.append(linha);
+            }
+
+            br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return buffer.toString();
+    }
+
+}
